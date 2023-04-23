5a2426e19b4e03488bffb7d1711f04ae8c6e4deb
cresb-49 <carlosbpac@gmail.com>
2023-04-23 17:17:02 -0600

    dependecias y uso de rutas de la api

--- Server/server-gamificacion/src/index.js
+++ Server/server-gamificacion/src/index.js
@@ -1,10 +1,46 @@
-const express = require('express')
+//Configuraciones del entorno del servidor
+const PORT = 5000;
+const DATABASE = 'gamificacion'
 
-const app = express()
+//Imports
+const express = require('express')
+const bp = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Configuracion de CORS
+const corsOptions = {
+    origin: 'http://localhost:8080', //TODO: puerto del forntend
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+const app = express();
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
 
 async function start() {
+    try {
+        const db = await mongoose.connect('mongodb://localhost:27017/' + DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            family: 4
+        });
+        console.log('Conectado a la base de datos: ', db.connection.name)
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+//Rutas de la api
+app.use('/api',require('./routes/usuario'));
+app.use('/api',require('./routes/juego'));
+
+//Inicio de la conexion con la base de datos
 start()
 
-app.listen(5000)
-console.log("Listening to port: 5000")
+//Inicio dels servidor con el puerto configurado
+app.listen(PORT)
+console.log("Listening to port: ",PORT)
