dfa701528ec316c3ab4e11f4bcc98689b32746be
cresb-49 <carlosbpac@gmail.com>
2023-05-01 00:17:48 -0600

    renombrado de variables y reutilizacion de codigo

--- Server/server-gamificacion/src/controllers/juego.js
+++ Server/server-gamificacion/src/controllers/juego.js
@@ -1,24 +1,9 @@
 const juego = require('../models/juego');
 
 const getJuego = async (req, res) => {
-    const codigo = req.query.codigo;
+    const codigo = req.query.juego;
     try {
-        const result = await juego.aggregate([
-            {
-                $match: { codigo: codigo }
-            },
-            {
-                $project: {
-                    _id: 1,
-                    codigo: 1,
-                    nombre: 1,
-                    autores: 1,
-                    like: 1,
-                    likes: { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } },
-                    dislikes: { $subtract: [{ $size: "$like" }, { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } }] }
-                }
-            }
-        ]);
+        const result = await obtenerJuego(codigo);
         if (result.length === 1) {
             res.status(200).json(result[0]);
         } else {
@@ -52,21 +37,27 @@ const getJuegos = async (req, res) => {
 
 const valorizarJuego = async (req, res) => {
     try {
-        const fillter = { codigo: req.body.codigo }
+        const fillter = { codigo: req.body.juego }
         const valorizacion = {
             usuario: req.body.usuario,
             like: req.body.like
         }
         const resultJuego = await juego.findOne(fillter);
-        const foundLike = await resultJuego.like.find(like => like.usuario === valorizacion.usuario);
-        if (foundLike) {
-            foundLike.like = valorizacion.like;
-            const result = await resultJuego.save()
-            res.status(200).json(result);
+        if (resultJuego) {
+            const foundLike = await resultJuego.like.find(like => like.usuario === valorizacion.usuario);
+            if (foundLike) {
+                foundLike.like = valorizacion.like;
+                await resultJuego.save();
+                const result = await obtenerJuego(fillter.codigo);
+                res.status(200).json(result[0]);
+            } else {
+                resultJuego.like.push(valorizacion)
+                await resultJuego.save();
+                const result = await obtenerJuego(fillter.codigo);
+                res.status(200).json(result[0]);
+            }
         } else {
-            resultJuego.like.push(valorizacion)
-            const result = await resultJuego.save()
-            res.status(200).json(result);
+            res.status(409).json({ error: `No existe un juego ${fillter.codigo}` });
         }
     } catch (error) {
         res.status(409).json({ error: error.message });
@@ -75,7 +66,7 @@ const valorizarJuego = async (req, res) => {
 
 const isLike = async (req, res) => {
     try {
-        const fillterJuego = { codigo: req.query.codigo }
+        const fillterJuego = { codigo: req.query.juego }
         const user = req.query.usuario
         const result = await juego.findOne(fillterJuego);
         if (result) {
@@ -86,7 +77,7 @@ const isLike = async (req, res) => {
                 res.status(409).json({ usuario: user, like: null });
             }
         } else {
-            res.status(409).json({ error: `No existe like del usuario ${user}` });
+            res.status(409).json({ error: `No existe el juego ${fillterJuego.codigo}` });
         }
     } catch (error) {
         res.status(409).json({ error: error.message });
@@ -94,29 +85,14 @@ const isLike = async (req, res) => {
 }
 
 const elimimarLike = async (req, res) => {
-    const data = { usuario: req.body.usuario, codigo: req.body.codigo }
+    const data = { usuario: req.body.usuario, codigo: req.body.juego }
     try {
         const resultJuego = await juego.findOne({ codigo: data.codigo });
         if (resultJuego) {
             const newLike = await resultJuego.like.filter(like => like.usuario !== data.usuario);
             resultJuego.like = newLike;
             await resultJuego.save();
-            const result = await juego.aggregate([
-                {
-                    $match: { codigo: data.codigo }
-                },
-                {
-                    $project: {
-                        _id: 1,
-                        codigo: 1,
-                        nombre: 1,
-                        autores: 1,
-                        like: 1,
-                        likes: { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } },
-                        dislikes: { $subtract: [{ $size: "$like" }, { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } }] }
-                    }
-                }
-            ]);
+            const result = await obtenerJuego(data.codigo);
             res.status(200).json(result[0]);
         } else {
             res.status(409).json({ error: `No existe el juego ${data.codigo}` });
@@ -124,6 +100,26 @@ const elimimarLike = async (req, res) => {
     } catch (error) {
         res.status(409).json({ error: error.message });
     }
+}
+
+const obtenerJuego = async (codigo) => {
+    const result = await juego.aggregate([
+        {
+            $match: { codigo: codigo }
+        },
+        {
+            $project: {
+                _id: 1,
+                codigo: 1,
+                nombre: 1,
+                autores: 1,
+                like: 1,
+                likes: { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } },
+                dislikes: { $subtract: [{ $size: "$like" }, { $sum: { $map: { input: "$like", as: "v", in: { $cond: [{ $eq: ["$$v.like", true] }, 1, 0] } } } }] }
+            }
+        }
+    ]);
+    return result;
 }
 
 module.exports = {
