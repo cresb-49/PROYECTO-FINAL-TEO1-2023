1cc1eb893fda6ebcb46ec12b1fc7226bb5bbde3e
FernandoJRR <fercitotron@gmail.com>
2023-05-07 15:47:58 -0600

    Agregar Sopa de Letras

--- App/app-gamificacion/src/app/sopa/sopa.module.ts
+++ App/app-gamificacion/src/app/sopa/sopa.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { SopaRoutingModule } from './sopa-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
+import { JuegoSopaLetrasComponent } from './game/juego-sopa-letras/juego-sopa-letras.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    JuegoSopaLetrasComponent
   ],
   imports: [
     CommonModule,
-    SopaRoutingModule
+    SopaRoutingModule,
+    FormsModule
   ]
 })
 export class SopaModule { }
