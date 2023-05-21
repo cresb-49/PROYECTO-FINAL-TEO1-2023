75104357bd780eaf79e11dd66aa245529b96565e
cresb-49 <carlosbpac@gmail.com>
2023-05-20 21:46:21 -0600

    vista del ranking

--- App/app-gamificacion/src/app/shared/ranking-juego/ranking-juego.component.ts
+++ App/app-gamificacion/src/app/shared/ranking-juego/ranking-juego.component.ts
@@ -10,6 +10,7 @@ export class RankingJuegoComponent implements OnInit {
   @Input() juego!: string;
 
   public ranking!: any[];
+  public parametros!: string[];
 
   constructor(private rankingService: RankingService) { }
 
@@ -19,6 +20,7 @@ export class RankingJuegoComponent implements OnInit {
       {
         next: (response: any[]) => {
           console.log(response);
+          this.ranking = response;
         },
         error: (error) => {
           console.log(error);
@@ -30,5 +32,29 @@ export class RankingJuegoComponent implements OnInit {
 
   public ObjToString(ranking: any): string {
     return JSON.stringify(ranking);
+  }
+
+  public calcularParametros(item: any): any {
+    let tmp: string[] = [];
+    if (this.juego === 'J00001') {
+      tmp.push(`Partidas Jugadas: ${item.jugado}`);
+      tmp.push(`Movimientos de Mas: ${item.movimientosDeMas}`);
+      tmp.push(`Tiempo: ${item.tiempo}`);
+      return tmp;
+    } else if (this.juego === 'J00002') {
+      tmp.push(`Punteo: ${item.punteo}`);
+      tmp.push(`Partidas Jugadas: ${item.jugado}`);
+      tmp.push(`Palabras Encontradas: ${item.palabrasEncontradas}`);
+      tmp.push(`Palabras Falladas: ${item.palabrasFalladas}`);
+      return tmp;
+    } else if (this.juego === 'J00003') {
+      tmp.push(`Partidas Jugadas: ${item.jugado}`);
+      tmp.push(`Tiempo: ${item.tiempo}`);
+      return tmp;
+    } else if (this.juego === 'J00004') {
+      tmp.push(`Partidas Jugadas: ${item.jugado}`);
+      tmp.push(`Tiempo: ${item.tiempo}`);
+      return tmp;
+    }
   }
 }
